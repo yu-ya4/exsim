b036aecb284399eb897b2ed76ca893a36d75488e
yu <yuya4.matsumura@gmail.com>
2016-09-06 12:47:21 +0900

    show similar actions

--- word2vec.py
+++ word2vec.py
@@ -1,13 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from gensim.models import word2vec
+import sys
 
 
 if __name__ == '__main__':
 
-    data1 = word2vec.Text8Corpus('./docs/data1.txt')
-    model1 = word2vec.Word2Vec(data1, size=200)
+    data = word2vec.Text8Corpus('./docs/data2.txt')
+    model = word2vec.Word2Vec(data, size=200, window=15)
+    model.save('test3.model')
 
-    out=model.most_similar(positive=['飲む'], topn=100)
-    for x in out:
-        print(x[0],x[1])
+    action_list = []
+    f = open('./actions.txt', 'r')
+    for line in f:
+        action = line.replace('\n', '')
+        action = action.replace('"', '')
+        action_list.append(action)
+    f.close()
+
+    print('ok')
+
+    for query in sys.stdin:
+        if query in action_list:
+            # similar_actions = []
+            out=model.most_similar(positive=[query], topn=10000)
+            for x in out:
+                if x[0] in action_list:
+                    print(x[0],x[1])
