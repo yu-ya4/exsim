a5f1a69bb7502d86861cf806683c2ce1bf21372b
yu-ya4 <yuya4.matsumura@gmail.com>
2017-09-12 00:59:44 +0900

    Update README.md

--- README.md
+++ README.md
@@ -12,51 +12,50 @@
 ```
 doc = Document()
 # reviews
-doc.make_text_file_from_database(0, './docs/tabelog/20170816/reviews.txt')
+doc.make_text_file_from_database(0, '../../data/docs/tabelog/20170816/reviews.txt')
 # restaurant_prs
-doc.make_text_file_from_database(1, './docs/tabelog/20170816/restaurant_prs.txt')
+doc.make_text_file_from_database(1, '../../data/docs/tabelog/20170816/restaurant_prs.txt')
 # reviews and restaurant_prs
-doc.make_text_file_from_database(2, './docs/tabelog/20170816/reviews_and_restaurant_prs.txt')
+doc.make_text_file_from_database(2, '../../data/docs/tabelog/20170816/reviews_and_restaurant_prs.txt')
 ```
 
 ## Make divided document
 ```
 doc = Document()
-doc.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
 # テキストファイルを読み込み分かち書きする．メンバ変数に保存．
-doc.make_document('./docs/tabelog/20170816/reviews.txt')
+doc.make_document('../../data/docs/tabelog/20170816/reviews.txt')
 # メンバ変数に保存された分かち書きされた文章をテキストファイルに書き出す
-doc.write_document('./docs/tabelog/20170816/reviews_divided.txt')
+doc.write_document('../../data/docs/tabelog/20170816/reviews_divided.txt')
 ```
 
 ## Replace experiences for symbols
 ```
 doc = Document()
-doc.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
+doc.read_experience_list('chie-extracted2')
 # 分かち書きされた文章を読み込みメンバ変数に保存.
-doc.read_document('./docs/tabelog/20170816/reviews_divided.txt')
-# 「飲む」という語に注目．「飲む」の10語以内にある経験をなす語(actionsに含まれる語)を記号に置き換える．
-doc.replace_actions('飲む', 10)
-doc.write_document('./docs/tabelog/20170816/reviews_replaced_10.txt')
+doc.read_document('../../data/docs/tabelog/20170816/reviews_divided.txt')
+# 「飲む」という語に注目．「飲む」の10語以内にある経験をなす語(experiencesに含まれる語)を記号に置き換える．
+doc.replace_experiences('飲む', 10)
+doc.write_document('../../data/docs/tabelog/20170816/reviews_replaced_10.txt')
 ```
 
 ## Weight experience words
 
 ```
 doc = Document()
-doc.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
-doc.read_document('./docs/tabelog/20170816/reviews_actions_20170607飲むcut_replaced_10.txt')
+doc.read_experience_list('chie-extracted2')
+doc.read_document('../../data/docs/tabelog/20170816/reviews_actions_20170607飲むcut_replaced_10.txt')
 # 経験にあたる語を2つずつ増やす
-doc.weight_actions(2)
-doc.write_document('./docs/tabelog/20170816/reviews_actions_20170607飲むcut_replaced_10_three.txt')
+doc.weight_experiences(2)
+doc.write_document('../../data/docs/tabelog/20170816/reviews_actions_20170607飲むcut_replaced_10_three.txt')
 ```
 
 ## Get words around the target word
 
 ```
 doc = Document()
-doc.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
-doc.read_document('./docs/tabelog/20170816/reviews_actions_20170607飲むcut_replaced_10.txt')
-doc.get_words_around_actions(15)
-print(sorted(doc.words_around_actions['ちょっと'].items(), key = lambda x: x[1]))
+doc.read_experience_list('chie-extracted2')
+doc.read_document('../../data/docs/test/test-divided-replaced-10.txt')
+doc.get_words_around_experiences(15)
+doc.show_words_around_experience('ちょっと')
 ```
--- main.py
+++ main.py
@@ -4,59 +4,52 @@
 from document import Document
 
 if __name__ == '__main__':
-    doc = Document()
-    doc.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
-    doc.read_document('../act-geo-matrix/reviews/20170607/all_text_飲む_replaced_50.txt')
-    doc.weight_actions(2)
-    doc.write_document('../act-geo-matrix/reviews/20170607/all_text_飲む_replaced_50_three.txt')
-    exit()
-
-    # doc5 = Document()
-    # doc5.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
-    # doc5.make_document('../act-geo-matrix/reviews/20170607/all_text_飲む.txt')
-    # doc5.replace_actions('飲む', 5)
-    # doc5.write_document('../act-geo-matrix/reviews/20170607/all_text_飲む_replaced_5.txt')
-    #
-    # doc10 = Document()
-    # doc10.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
-    # doc10.make_document('../act-geo-matrix/reviews/20170607/all_text_飲む.txt')
-    # doc10.replace_actions('飲む', 10)
-    # doc10.write_document('../act-geo-matrix/reviews/20170607/all_text_飲む_replaced_10.txt')
-    #
-    # doc15 = Document()
-    # doc15.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
-    # doc15.make_document('../act-geo-matrix/reviews/20170607/all_text_飲む.txt')
-    # doc15.replace_actions('飲む', 15)
-    # doc15.write_document('../act-geo-matrix/reviews/20170607/all_text_飲む_replaced_15.txt')
-    #
-    # doc20 = Document()
-    # doc20.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
-    # doc20.make_document('../act-geo-matrix/reviews/20170607/all_text_飲む.txt')
-    # doc20.replace_actions('飲む', 20)
-    # doc20.write_document('../act-geo-matrix/reviews/20170607/all_text_飲む_replaced_20.txt')
-    #
-    # doc25 = Document()
-    # doc25.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
-    # doc25.make_document('../act-geo-matrix/reviews/20170607/all_text_飲む.txt')
-    # doc25.replace_actions('飲む', 25)
-    # doc25.write_document('../act-geo-matrix/reviews/20170607/all_text_飲む_replaced_25.txt')
-    #
-    # doc50 = Document()
-    # doc50.read_action_list('../act-geo-matrix/actions/20170607飲むcut.txt')
-    # doc50.make_document('../act-geo-matrix/reviews/20170607/all_text_飲む.txt')
-    # doc50.replace_actions('飲む', 50)
-    # doc50.write_document('../act-geo-matrix/reviews/20170607/all_text_飲む_replaced_50.txt')
 
 
     # doc = Document()
-    # doc.read_action_list('../act-geo-matrix/actions/action_飲む_extended.txt')
-    # # doc.make_document('../act-geo-matrix/reviews/search_test/大人数で朝まで飲む/bodies.txt')
-    # doc.make_document('../act-geo-matrix/reviews/yolp/kyoto/bodies.txt')
-    # doc.replace_actions('飲む', 100)
-    # doc.write_document('../act-geo-matrix/reviews/yolp/kyoto/all_bodies_replaced_100.txt')
-    # print(doc.document)
-    # rep_dic = doc.make_replace_dict()
-    # print(rep_dic)
+    # doc.read_action_list('../act-geo-matrix/actions/20170816/20170816test-actions.txt')
+    # doc.read_document('./docs/tabelog/20170816/reviews_divided.txt')
+    # # words = ['カウンター' 'ちょっと', 'オシャレ']
+    #
+    # # doc.weight_words(4, words)
+    # doc.replace_actions('飲む', 15)
+    # doc.write_document('./docs/tabelog/20170816/reviews_actions_20170816test_replaced_15.txt')
+    # exit()
+    # doc = Document()
+    # doc.make_text_file_from_database(0, '../../data/docs/test/test.txt')
+    # exit()
+
+    # doc = Document()
+    # doc.make_document('../../data/docs/test/test.txt')
+    # # words = ['カウンター' 'ちょっと', 'オシャレ']
+    # doc.write_document('../../data/docs/test/test-divided.txt')
+    # exit()
+
+    # doc = Document()
+    # doc.read_experience_list('chie-extracted2')
+    # doc.read_document('../../data/docs/test/test-divided.txt')
+    # doc.replace_experiences('飲む', 10)
+    # doc.write_document('../../data/docs/test/test-divided-replaced-10.txt')
+    # exit()
+
+    # doc = Document()
+    # doc.read_experience_list('chie-extracted2')
+    # doc.read_document('../../data/docs/test/test-divided-replaced-10.txt')
+    # doc.weight_experiences(2)
+    # doc.write_document('../../data/docs/test/test-divided-replaced-10-three.txt')
+    # exit()
+    # words = ['カウンター' 'ちょっと', 'オシャレ']
+
+    # doc.weight_words(4, words)
+
+    doc = Document()
+    doc.read_experience_list('chie-extracted2')
+    doc.read_document('../../data/docs/test/test-divided-replaced-10.txt')
+    doc.get_words_around_experiences(15)
+    doc.show_words_around_experience('ちょっと')
+    exit()
+
+
 
     # all_text = ''
     # fw = open('../act-geo-matrix/reviews/search_test/all_bodies.txt', 'w')
